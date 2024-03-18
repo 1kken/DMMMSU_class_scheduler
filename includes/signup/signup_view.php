37b0c1b31d7f8ddde7a3e8d620a5dfeacf5fe616
1ikken <tavorearljohn@gmail.com>
2024-03-18 17:24:13 +0800

    add user id taken

--- includes/signup/signup_view.php
+++ includes/signup/signup_view.php
@@ -3,7 +3,7 @@
         if(isset($_SESSION["errors_signup"])){
             $errors = $_SESSION["errors_signup"];
             foreach($errors as $error){
-                echo "<p class='dark:text-red-500 text-xs italic'>$error</p>";
+                echo "<p class='dark:text-white text-xs'>$error</p>";
             }
         }
         unset($_SESSION["errors_signup"]);
