6f2fef998096bb8ed44e7fc56fd249a96a6da9e5
1ikken <tavorearljohn@gmail.com>
2024-04-05 10:01:01 +0800

    database

--- db_des/backup.sql
+++ db_des/backup.sql
@@ -18,21 +18,16 @@ USE `class_schedule`;
 
 /*Table structure for table `instructor` */
 
-DROP TABLE IF EXISTS `instructor`;
-
 CREATE TABLE `instructor` (
   `instructor_id` varchar(10) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `first_name` varchar(50) NOT NULL,
-  `middle_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`instructor_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `instructor` */
-
 /*Table structure for table `rooms` */
-
-DROP TABLE IF EXISTS `rooms`;
 
 CREATE TABLE `rooms` (
   `room_id` varchar(25) NOT NULL,
@@ -41,48 +36,37 @@ CREATE TABLE `rooms` (
   PRIMARY KEY (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `rooms` */
-
 /*Table structure for table `schedule` */
-
-DROP TABLE IF EXISTS `schedule`;
 
 CREATE TABLE `schedule` (
   `schedule_id` int(11) NOT NULL AUTO_INCREMENT,
   `code_no` text NOT NULL,
-  `student_record_id` int(11) NOT NULL,
   `room_id` varchar(25) NOT NULL,
   `instructor_id` varchar(10) NOT NULL,
   `day` varchar(20) NOT NULL,
   `time` time NOT NULL,
   `subject_id` varchar(20) NOT NULL,
+  `section_id` varchar(2) NOT NULL,
+  `sy` varchar(9) NOT NULL,
   PRIMARY KEY (`schedule_id`),
-  KEY `student_record_id` (`student_record_id`),
   KEY `room_id` (`room_id`),
   KEY `subject_id` (`subject_id`),
   KEY `instructor_id` (`instructor_id`),
-  CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`student_record_id`) REFERENCES `student_record` (`student_record_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  KEY `section_id` (`section_id`),
   CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `schedule_ibfk_3` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `schedule_ibfk_4` FOREIGN KEY (`instructor_id`) REFERENCES `instructor` (`instructor_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `schedule_ibfk_4` FOREIGN KEY (`instructor_id`) REFERENCES `instructor` (`instructor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `schedule_ibfk_5` FOREIGN KEY (`section_id`) REFERENCES `section` (`section_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `schedule` */
-
 /*Table structure for table `section` */
-
-DROP TABLE IF EXISTS `section`;
 
 CREATE TABLE `section` (
   `section_id` varchar(2) NOT NULL,
   PRIMARY KEY (`section_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `section` */
-
 /*Table structure for table `student` */
-
-DROP TABLE IF EXISTS `student`;
 
 CREATE TABLE `student` (
   `student_id` varchar(10) NOT NULL,
@@ -90,20 +74,16 @@ CREATE TABLE `student` (
   `first_name` varchar(50) NOT NULL,
   `middle_name` varchar(50) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
-  `section_id` varchar(2) DEFAULT NULL,
   PRIMARY KEY (`student_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `student` */
-
 /*Table structure for table `student_record` */
-
-DROP TABLE IF EXISTS `student_record`;
 
 CREATE TABLE `student_record` (
   `student_record_id` int(11) NOT NULL AUTO_INCREMENT,
   `student_id` varchar(10) NOT NULL,
   `section_id` varchar(2) DEFAULT NULL,
+  `sy` varchar(9) NOT NULL,
   PRIMARY KEY (`student_record_id`),
   KEY `student_id` (`student_id`),
   KEY `section_id` (`section_id`),
@@ -111,11 +91,7 @@ CREATE TABLE `student_record` (
   CONSTRAINT `student_record_ibfk_2` FOREIGN KEY (`section_id`) REFERENCES `section` (`section_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `student_record` */
-
 /*Table structure for table `subject` */
-
-DROP TABLE IF EXISTS `subject`;
 
 CREATE TABLE `subject` (
   `subject_id` varchar(20) NOT NULL,
@@ -125,25 +101,20 @@ CREATE TABLE `subject` (
   PRIMARY KEY (`subject_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `subject` */
-
 /*Table structure for table `user` */
-
-DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(10) NOT NULL,
+  `user_type` varchar(10) NOT NULL,
   `student_id` varchar(10) DEFAULT NULL,
   `instructor_id` varchar(10) DEFAULT NULL,
+  `password` text NOT NULL,
   PRIMARY KEY (`user_id`),
   KEY `student_id` (`student_id`),
   KEY `instructor_id` (`instructor_id`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user_ibfk_2` FOREIGN KEY (`instructor_id`) REFERENCES `instructor` (`instructor_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-/*Data for the table `user` */
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
