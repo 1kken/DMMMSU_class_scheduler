74b8e217213d8cdc8888e491881e231694e115a0
1ikken <tavorearljohn@gmail.com>
2024-05-16 18:25:15 +0800

    fix inex.php

--- includes/dashboard_model.php
+++ includes/dashboard_model.php
@@ -3,7 +3,7 @@ require_once("database_header.php");
 require_once('config_session.inc.php');
 
 if(!isset($_SESSION["user_id"])){
-    header("LOCATION: ./index.php");
+    header("LOCATION: ../index.php");
     exit();
 }
 
